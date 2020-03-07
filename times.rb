c883b8171abaae73086393c380c25ff66e4413c7
Pablo Menz <p.menz.r@gmail.com>
2020-03-07 18:44:21 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -5,3 +5,8 @@ end
 
 
 
+#We're still struggling to master that levitation charm.
+#Since we need to keep practicing, let's write some code 
+#that allows us to `puts` the phrase "Wingardium Leviosa"
+#only seven times (instead of infinitely, like when we 
+#used just a plain `loop` construct).
